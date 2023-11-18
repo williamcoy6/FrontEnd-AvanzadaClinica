e79480ce86370fc1b6a4e33e418438e6d384299e
SUPERJUAN123456 <juanesmosquera23@gmail.com>
2023-11-18 14:32:21 -0500

    CREAR MEDICO HTML Y EDITAR PERFIL HTML EN SI LISTo

--- my-app/src/app/pagina/crear-medico/crear-medico.component.ts
+++ my-app/src/app/pagina/crear-medico/crear-medico.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { ClinicaService } from 'src/app/servicios/clinica.service';
+import { Alerta } from '../alerta/alerta.component';
+import { ImagenService } from 'src/app/servicios/imagen.service';
 
 @Component({
   selector: 'app-crear-medico',
@@ -6,5 +11,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-medico.component.css']
 })
 export class CrearMedicoComponent {
+  alerta!: Alerta;
+  ciudades: string[];
 
+
+  constructor(private imagenService: ImagenService, private authService: AuthService, private clinicaService: ClinicaService) {
+   this.ciudades = [];
+
+}
 }
